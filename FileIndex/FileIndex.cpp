7a668314352cd67209a0f9fc021f9926debfba10
Anikalogika4417 <anikalogika4417@gmail.com>
2023-11-19 23:02:15 +0200

    Read file

--- FileIndex/FileIndex.cpp
+++ FileIndex/FileIndex.cpp
@@ -2,19 +2,38 @@
 //
 
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+
+namespace fs = std::filesystem;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const fs::path directoryPath{ "Downloads" };
+
+    for (const auto& entry : fs::directory_iterator(directoryPath))
+    {
+        if (fs::is_regular_file(entry))
+        {
+            std::cout << "File: " << entry.path() << std::endl;
+
+            // Add your code to read the file here
+            std::ifstream file(entry.path());
+            if (file.is_open())
+            {
+                std::string line;
+                while (std::getline(file, line))
+                {
+                    std::cout << line << std::endl;
+                }
+                file.close();
+            }
+            else
+            {
+                std::cerr << "Unable to open file: " << entry.path() << std::endl;
+            }
+        }
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
